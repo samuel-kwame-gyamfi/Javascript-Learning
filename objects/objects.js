d74ad08b1c0382bfba5a00a324b12cf9f720832e
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2026-01-05 13:28:58 +0000

    the basics of object

--- objects/objects.js
+++ objects/objects.js
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-</head>
-<body>
-    
-    <script src="objects.js"></script>
-</body>
-</html>
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  isvisible: true,
+  draw: function () {
+    console.log("draw");
+  },
+};
+
+circle.draw();
